81a3c7e1629e7f737474f83f90cfaab541fb06db
Stavera21 <santiagotaveravargas@yahoo.com>
2023-08-03 17:22:36 -0500

    Operaciones matematicas con 2 numeros

---
+++ operaciones.py
@@ -0,0 +1,12 @@
+num1 = int(input("ingrese un primer numero: "))
+num2 = int(input("Ingrese un segundo numero: "))
+
+suma = num1 + num2
+resta = num1 - num2
+multi = num1 * num2
+division = num1 / num2
+
+print(f"La suma de ambos numeros es: {suma}")
+print(f"La resta de ambos numeros es: {resta}")
+print(f"La multiplicacion de ambos numeros es: {multi}")
+print(f"La division de ambos numeros es: {division}")
