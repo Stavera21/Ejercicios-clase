aae31add36d80128969a01ae5ce761d9ccf86a3c
Stavera21 <santiagotaveravargas@yahoo.com>
2023-08-02 23:37:37 -0500

    Lista ivertida

---
+++ orden_lista.py
@@ -0,0 +1,10 @@
+def invertir_lista(lista):
+    lista.reverse()
+    return lista
+def invertir_lista(lista):
+    return lista[::-1]
+lista_original = [1, 2, 3, 4, 5]
+lista_invertida = invertir_lista(lista_original)
+
+print("Lista original:", lista_original)
+print("Lista invertida:", lista_invertida)
